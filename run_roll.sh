d34e3bfb76635173ad433181f5de7fbf5e4eceb5
root <root@dgx-44.cm.cluster>
2025-06-06 20:59:27 +0800

    enable multi tenant

--- run_roll.sh
+++ run_roll.sh
@@ -2,4 +2,4 @@
 # VLLM_USE_PRECOMPILED=1 pip install --editable .
 NCG_IMAGE=roll-registry.cn-hangzhou.cr.aliyuncs.com\#roll/pytorch:nvcr-24.05-py3-torch260-vllm084
 LOCAL_IMAGE=/home/twubt/containers/roll_new.sqsh
-srun --account lsdisttrain --nodes 1 --gpus-per-node 4 --no-container-mount-home --container-remap-root --container-mounts=/home/twubt/.cache/huggingface:/root/.cache/huggingface,/home/twubt/workspace:/workspace --container-workdir=/workspace --container-writable --container-image $LOCAL_IMAGE --pty bash
+srun --account lsdisttrain --nodes 1 --gpus-per-node 2 --no-container-mount-home --container-remap-root --container-mounts=/home/twubt/.cache/huggingface:/root/.cache/huggingface,/home/twubt/workspace:/workspace --container-workdir=/workspace --container-writable --container-image $LOCAL_IMAGE --pty bash
--- tests/distributed/executor/test_gpu_id.py
+++ tests/distributed/executor/test_gpu_id.py
@@ -1,28 +1,52 @@
+import copy
+from typing import Any, List
+
+import pytest
+import ray
+import time
+import torch
+import os
+from roll.configs.worker_config import WorkerConfig
+from roll.distributed.executor.cluster import Cluster
+from roll.distributed.executor.worker import Worker, RankInfo
+from roll.distributed.scheduler.decorator import register, Dispatch
+from roll.distributed.scheduler.resource_manager import ResourceManager
+
+
+class TestWorker(Worker):
+    def __init__(self, worker_config: WorkerConfig):
+        super().__init__(worker_config=worker_config)
+        self.value = self.rank
+        x = torch.zeros(1000*1000*1000, dtype=torch.float32, device="cuda:0")
+        y = torch.zeros(1000*1000*1000, dtype=torch.float32, device="cuda:1")
+        print(f"Worker {self.rank} initialized with value {self.value} and GPUs {os.environ.get('CUDA_VISIBLE_DEVICES', 'None')}")
+
+
+    @register(dispatch_mode=Dispatch.ONE_TO_ALL)
+    def add(self, x):
+        print("Adding value:", x, "to worker rank:", self.rank)
+        return
+
 def test_gpu_id():
     ray.init(log_to_driver=True)
 
-    resource_manager = ResourceManager()
+    print(f"Available: {ray.available_resources()}")
 
-    test_worker_config = WorkerConfig(name="test_worker", world_size=8)
+    resource_manager = ResourceManager(num_nodes=1, num_gpus_per_node=2)
+
+    test_worker_config = WorkerConfig(name="test_worker", world_size=1, num_gpus_per_worker=2, device_mapping="[0,1]")
     test_cluster: Any = Cluster(
         name=test_worker_config.name,
         resource_manager=resource_manager,
-        worker_cls=TestDPWorker,
+        worker_cls=TestWorker,
         worker_config=test_worker_config,
     )
+    time.sleep(60)
     x = [1, 2, 3, 4, 5, 6, 7, 8]
     res = test_cluster.add(x=x)
     print(res)
-    assert res == [
-        [0, 0, 1, 1],
-        [0, 0, 1, 2],
-        [0, 0, 1, 3],
-        [0, 0, 1, 4],
-        [1, 0, 1, 5],
-        [1, 0, 1, 6],
-        [1, 0, 1, 7],
-        [1, 0, 1, 8],
-    ]
+    
+
 
 
 if __name__ == "__main__":
