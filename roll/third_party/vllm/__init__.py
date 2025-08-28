11332c6baa8f57eb568f1c942e10a1e9a3be5c0c
松菊 <songju.wty@alibaba-inc.com>
2025-08-28 21:56:08 +0800

    add device affinity, enhance error handling of migration scheduler

--- roll/third_party/vllm/__init__.py
+++ roll/third_party/vllm/__init__.py
@@ -12,9 +12,6 @@ elif "0.8.4" in vllm.__version__:
     LLM = Llm084
     AsyncLLM = AsyncLLM084
 else:
-    from roll.third_party.vllm.vllm_0_8_4.llm import Llm084
-    from roll.third_party.vllm.vllm_0_8_4.v1.async_llm import AsyncLLM084
-    LLM = Llm084
-    AsyncLLM = AsyncLLM084
+    raise NotImplementedError(f"Scale aligner version {vllm.__version__} is not supported.")
 
 __all__ = ["LLM", "AsyncLLM"]
