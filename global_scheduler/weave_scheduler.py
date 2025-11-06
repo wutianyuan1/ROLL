b99b6e4471f850d85e592b2fa31b8cf2d4675cc5
caolunxi.clx <caolunxi.clx@alibaba-inc.com>
2025-11-06 10:42:50 +0800

    init wild_trace_sim, weave scheduler supports 32B model with 2 rollout nodes and slowdown trace of each job

--- global_scheduler/weave_scheduler.py
+++ global_scheduler/weave_scheduler.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
-from typing import List, Dict, Callable
+from typing import List, Dict, Callable, Tuple
+from datetime import datetime
 from global_scheduler.structs import Job, JobGroup
 from global_scheduler.new_simulator import WeaveSimulator
 from global_scheduler.baselines import BaselineScheduler
@@ -27,13 +28,26 @@ def per_time_cost(jobs: List[Job], num_rollout_nodes: int, train_busy_times: Dic
         return cost_per_time * 1, invalid_jobs
 
 
+def get_slowdowns(jobs: List[Job], total_time: float, train_busy_times: Dict):
+    '''
+    Calculate the slowdown of each involved job.
+    '''
+    jid_2_sld: Dict[str, float] = {}
+    for job in jobs:
+        num_iters = len(train_busy_times['TN'][job.job_id])
+        jid_2_sld[job.job_id] = (total_time / num_iters) / (job.t_rollout + job.t_train)
+    return jid_2_sld
+
+
 class WeaveScheduler(BaselineScheduler):
     def __init__(self, cost_func: Callable[[Dict], float], max_group_size: int = 5,
                  simulate_steps: int = 100, rollout_cost: float = 1/3, train_cost: float = 1.0):
         super().__init__(cost_func, max_group_size, simulate_steps, rollout_cost, train_cost)
+        # key: job_id, value: List[(slowdown, timing)]
+        self.jid_2_sld_trace: Dict[str, List[Tuple[float, datetime]]] = {}
 
-    def add_job(self, job: Job):
-        best_rollout_node, best_train_node, best_group, best_cost_delta = None, None, None, float("inf")
+    def add_job(self, job: Job, timing: datetime, double_rollout_resource: bool):
+        best_rollout_nodes, best_train_node, best_group, best_cost_delta, best_slowdowns = None, None, None, float("inf"), None
         for job_group_name, job_group in self.job_groups.items():
             if len(job_group.jobs) >= self.max_group_size:
                 continue
@@ -41,64 +55,109 @@ class WeaveScheduler(BaselineScheduler):
 
             # Case-1: share train, share rollout (direct insert)
             for train_node in all_train_nodes:
-                for rollout_node in all_rollout_nodes:
+                if double_rollout_resource and len(all_rollout_nodes) < 2:
+                    break
+                if not double_rollout_resource:
+                    candidate_rollout_nodes = [[node] for node in all_rollout_nodes]
+                else:
+                    candidate_rollout_nodes = [[all_rollout_nodes[i], all_rollout_nodes[j]] \
+                                               for i in range(len(all_rollout_nodes)) for j in range(i, len(all_rollout_nodes))]
+                for rollout_nodes in candidate_rollout_nodes:
                     tmp_job = deepcopy(job)
-                    tmp_job.rollout_nodes = [rollout_node]
+                    tmp_job.rollout_nodes = rollout_nodes
                     tmp_job.train_nodes = [train_node]
 
                     jobs_in_group = job_group.jobs + [tmp_job]
                     sim = WeaveSimulator(jobs_in_group)
                     rollout_busy_times, train_busy_times, utils, total_time = sim.simulate_run(self.simulate_steps)
                     cost = self.cost_func(jobs_in_group, len(all_rollout_nodes), train_busy_times, total_time, self.rollout_cost, self.train_cost)
+                    slowdowns = get_slowdowns(jobs_in_group, total_time, train_busy_times)
                     # print(f"Case-1, {rollout_node=}, {train_node=}, {cost=}")
                     if cost - self.group_costs[job_group_name] < best_cost_delta:
                         best_cost_delta = cost - self.group_costs[job_group_name]
-                        best_rollout_node = rollout_node
+                        best_rollout_nodes = rollout_nodes
                         best_train_node = train_node
                         best_group = job_group
+                        best_slowdowns = slowdowns
             # Case-2: share train, but scale-up to an individual rollout
             for train_node in all_train_nodes:
-                rollout_node = job_group.next_rollout_node_id()
+                if not double_rollout_resource:
+                    rollout_nodes = [job_group.next_rollout_node_id()]
+                else:
+                    rollout_nodes = [job_group.next_rollout_node_id(), job_group.next_rollout_node_id()]
                 tmp_job = deepcopy(job)
-                tmp_job.rollout_nodes = [rollout_node]
+                tmp_job.rollout_nodes = rollout_nodes
                 tmp_job.train_nodes = [train_node]
                 jobs_in_group = job_group.jobs + [tmp_job]
                 sim = WeaveSimulator(jobs_in_group)
                 rollout_busy_times, train_busy_times, utils, total_time = sim.simulate_run(self.simulate_steps)
                 cost = self.cost_func(jobs_in_group, len(all_rollout_nodes) + 1, train_busy_times, total_time, self.rollout_cost, self.train_cost)
+                slowdowns = get_slowdowns(jobs_in_group, total_time, train_busy_times)
                 # print(f"Case-2, {rollout_node=}, {train_node=}, {cost=}")
                 if cost - self.group_costs[job_group_name] < best_cost_delta:
                     best_cost_delta = cost - self.group_costs[job_group_name]
-                    best_rollout_node = rollout_node
+                    best_rollout_nodes = rollout_nodes
                     best_train_node = train_node
                     best_group = job_group
+                    best_slowdowns = slowdowns
         # Case-3: form a new group
         tmp_job = deepcopy(job)
-        tmp_job.rollout_nodes = ["0"]
+        tmp_job.rollout_nodes = ["0"] if not double_rollout_resource else ["0", "1"]
         tmp_job.train_nodes = ["TN"]
         job_group = JobGroup(self.next_group_id(), [tmp_job])
+        if double_rollout_resource:
+            # Manually set last_rollout_node_id 
+            job_group.last_rollout_node_id += 1
         sim = WeaveSimulator(job_group.jobs)
         rollout_busy_times, train_busy_times, utils, total_time = sim.simulate_run(self.simulate_steps)
         cost = self.cost_func(job_group.jobs, 1, train_busy_times, total_time, self.rollout_cost, self.train_cost)
+        slowdowns = get_slowdowns(job_group.jobs, total_time, train_busy_times)
         # print(f"Case-3, {cost=}")
         # If new group is the best
         if cost < best_cost_delta:
             best_cost_delta = cost
-            best_rollout_node = job_group.all_rollout_nodes[0]
+            best_rollout_nodes = job_group.all_rollout_nodes
             best_train_node = job_group.all_train_nodes[0]
             best_group = job_group
+            best_slowdowns = slowdowns
             self.job_groups[job_group.group_id] = job_group
             self.group_costs[job_group.group_id] = cost
         # If one existing group is the best, then assign this job to the group
         else:
             tmp_job = deepcopy(job)
-            tmp_job.rollout_nodes = [best_rollout_node]
+            tmp_job.rollout_nodes = best_rollout_nodes
             tmp_job.train_nodes = [best_train_node]
             self.last_group_id -= 1  # new group is not the best, recall the added group ID
             self.job_groups[best_group.group_id].jobs.append(tmp_job)
             self.group_costs[best_group.group_id] += best_cost_delta
         # print(self.group_costs)
-        return best_rollout_node, best_train_node, best_group, best_cost_delta, {}
+        # Update the slowdowns of relative jobs
+        for jid, sld in best_slowdowns.items():
+            self.jid_2_sld_trace.setdefault(jid, [])
+            self.jid_2_sld_trace[jid].append((sld, timing))
+        return best_rollout_nodes, best_train_node, best_group, best_cost_delta, {}
+
+    def remove_job(self, job_id: str, timing: datetime):
+        # To add the end timing for slowdown calculating.
+        self.jid_2_sld_trace[job_id].append((None, timing))
+        return super().remove_job(job_id)
+
+    def average_slowdown(self):
+        '''
+        slowdown_avg of a job = \sum \delta t_i * slowdown_i / (\sum \delta t_i)
+        '''
+        jid_2_avg_sld: Dict[str, float] = {}
+        for jid, sld_trace in self.jid_2_sld_trace.items():
+            assert sld_trace[-1][0] == None
+            delta_ts: List[float] = []
+            slds: List[float] = []
+            for i in range(1, len(sld_trace)):
+                delta_ts.append((sld_trace[i][1] - sld_trace[i - 1][1]).total_seconds())
+                slds.append(sld_trace[i - 1][0])
+            t = sum(delta_ts)
+            assert t == (sld_trace[-1][1] - sld_trace[0][1]).total_seconds()
+            jid_2_avg_sld[jid] = sum([delta_t * sld for delta_t, sld in zip(delta_ts, slds)]) / t
+        return jid_2_avg_sld
 
 
 if __name__ == "__main__":
---
+++ global_scheduler/wild_trace_sim.py
@@ -0,0 +1,199 @@
+import time
+import os
+import sys
+import random
+import numpy as np
+import json
+import math
+from tqdm import tqdm
+from copy import deepcopy
+from datetime import datetime
+from typing import Callable, List, Dict, Tuple
+from concurrent.futures import ProcessPoolExecutor
+
+from global_scheduler.brute_force_solver_new import BruteForceSolver
+from global_scheduler.weave_scheduler import WeaveScheduler, per_time_cost
+from global_scheduler.baselines import BaselineScheduler, RandomScheduler, MostIdleScheduler
+from global_scheduler.structs import Job
+
+
+def read_profile():
+    with open("global_scheduler/trace/profile.json", 'r') as f:
+        # profile_time['colo'/'disagg']['xB_xk']['generate'/'rollout']
+        profile_time: Dict[str, Dict[str, Dict[str, float]]] = json.load(f)
+    return profile_time
+
+
+def read_trace(trace_fn):
+    traces = []
+    with open(trace_fn, 'r') as f:
+        content = f.read().split("\n")
+    for line in content:
+        if len(line) <= 1:
+            continue
+        items = line.split(', ')
+        jid, t, event, job_type = items
+        jid = jid.strip("job")
+        t = datetime.strptime(t, '%Y-%m-%d %H:%M:%S')
+        event = int(event)
+        if event == 1:
+            traces.append([jid, t, event, job_type])
+        else:
+            traces.append([jid, t, event])
+    return traces
+
+
+def baseline(trace_fn: str, colo: bool):
+    location = 'colo' if colo else 'disagg'
+    profile_time = read_profile()
+    trace = read_trace(trace_fn)
+    jobs = {}
+    total_cost = 0
+    jid_2_num_steps = {}
+    for items in trace:
+        if len(items) > 3:
+            jid, t, event, job_type = items
+            assert event == 1 and jid not in jobs
+            t_rollout = profile_time[location][job_type]['generate']
+            t_train = profile_time[location][job_type]['train']
+            jobs[jid] = (t, t_rollout, t_train)
+        else:
+            jid, t, event = items
+            assert event == -1 and jid in jobs
+            t_start, t_rollout, t_train = jobs[jid]
+            duration = (t - t_start).total_seconds()
+            jid_2_num_steps[jid] = math.floor(duration / (t_rollout + t_train))
+            cost_per_sec = 1.0 if colo else (1/3 + 1.0)
+            if job_type.startswith('32B'):
+                cost_per_sec *= 2
+            total_cost += duration * cost_per_sec
+    return total_cost, jid_2_num_steps
+
+
+def sim_baseline(sched: BaselineScheduler, trace_fn: str, mannual_slo: float):
+    profile_time = read_profile()
+    trace = read_trace(trace_fn)
+    running_jobs: Dict[str, Job] = {}
+    total_cost, last_state_cost, last_t = 0, 0, trace[0][1]
+    last_state_total_jobs, last_state_invalid_jobs = 0, {}
+    time_costs = []  # [(timestamp, cost), ...]
+    time_invalid_jobs = []
+
+    for i, job_info in enumerate(tqdm(trace)):
+        jid, t, event = job_info[0], job_info[1], job_info[2]
+        if i > 0:
+            delta_t = (t - last_t).total_seconds()
+            total_cost += last_state_cost * delta_t
+            time_costs.append((last_t, t, last_state_cost))
+            time_invalid_jobs.append((last_t, t, last_state_invalid_jobs, last_state_total_jobs))
+        if event == 1:
+            assert len(job_info) == 4
+            job_type = job_info[3]
+            t_rollout = profile_time['disagg'][job_type]['generate']
+            t_train = profile_time['disagg'][job_type]['train']
+            job = Job(jid, t_rollout, t_train, mannual_slo)
+            running_jobs[jid] = job
+            # print(f"\n======== Insert Job {job.job_id} [{job.t_rollout=}, {job.t_train=}], {running_jobs=} ========")
+            sched.add_job(job, t, job_type.startswith('32B'))
+        else:
+            del running_jobs[jid]
+            # print(f"\n======== Delete Job {jid} [After {running_jobs=}] ========")
+            sched.remove_job(jid, t)
+        last_state_cost = sum(sched.group_costs.values())
+        last_state_invalid_jobs = deepcopy(sched.group_invalid_jobs)
+        last_state_total_jobs = sched.total_running_jobs
+        last_t = t
+
+    num_invalid_jobs = lambda group_invalid_jobs: sum(len(group_invalid_jobs[grp_id]) for grp_id in group_invalid_jobs)
+    slo_violation_counts = [num_invalid_jobs(i[2]) for i in time_invalid_jobs]
+    slo_violation_ratio = sum(slo_violation_counts) / sum(i[3] for i in time_invalid_jobs)
+    print(f"SLO violation ratio = {slo_violation_ratio}")
+    return total_cost, time_costs, time_invalid_jobs
+
+
+# Helper function for parallel execution
+def compute_opt_cost(jobs_list, max_group_size_):
+    solver = BruteForceSolver(jobs_list, max_group_size_, n_iters=20)
+    cost, _, _ = solver.solve(max_search_steps=10000)
+    return cost
+
+
+def sim_optimal(trace_fn: str, max_group_size: int, fallback_opt_cost: Dict, mannual_slo: float = -1):
+    trace = read_trace(trace_fn)
+    try:
+        print("Submitting all OPT computation tasks...")
+        executor = ProcessPoolExecutor(max_workers=8)
+        opt_tasks = []  # [(future, start_time, end_time), ...]
+        opt_current_jobs = {}
+        opt_last_t = trace[0][1]
+        for i, job_info in enumerate(trace):
+            jid, t, event = job_info[0], job_info[1], job_info[2]
+            if i > 0:
+                future = executor.submit(compute_opt_cost, [deepcopy(i) for i in opt_current_jobs.values()], max_group_size)
+                opt_tasks.append((future, opt_last_t, t))
+            if event == 1:
+                assert len(job_info) == 6
+                t_roll, t_train, slo = job_info[3], job_info[4], job_info[5]
+                opt_current_jobs[jid] = Job(jid, t_roll, t_train, slo if mannual_slo == -1 else mannual_slo)
+            else:
+                del opt_current_jobs[jid]
+            opt_last_t = t
+
+        print("Waiting for all OPT computations to complete...")
+        total_opt_cost = 0
+        opt_costs = []
+        for future, start_time, end_time in opt_tasks:
+            opt_cost = future.result()
+            real_opt = opt_cost
+            if opt_cost > 100000: # inf
+                opt_cost = fallback_opt_cost[start_time] # simply reset to the Weave's best value
+            delta_t = (end_time - start_time).total_seconds()
+            total_opt_cost += opt_cost * delta_t
+            opt_costs.append((start_time, end_time, opt_cost))
+            print(f"OPT cost for period {start_time} to {end_time}: {real_opt}, duration: {delta_t}")
+    finally:
+        executor.shutdown(wait=True)
+    return total_opt_cost, opt_costs
+
+
+def run_ablation_slo(trace_fn: str, max_group_size: int, slo: float):
+    weave = WeaveScheduler(per_time_cost, max_group_size)
+    total_cost, time_costs, time_invalid_jobs = sim_baseline(
+        weave,
+        trace_fn,
+        mannual_slo=slo
+    )
+    result_str = f"[{slo}] {total_cost=}\n"
+    print(result_str)
+    return total_cost, weave.average_slowdown()
+
+
+if __name__ == "__main__":
+    random.seed(2345)
+    np.random.seed(2345)
+    trace_fn = "global_scheduler/trace/wild.trace"
+    jid_2_duration = {}
+    tr = read_trace(trace_fn)
+    for items in tr:
+        jid, t = items[:2]
+        if jid not in jid_2_duration:
+            jid_2_duration[jid] = t
+        else:
+            jid_2_duration[jid] = (t - jid_2_duration[jid]).total_seconds()
+
+    colo_cost, colo_thpt = baseline(trace_fn, True)
+    naived_cost, naived_thpt = baseline(trace_fn, False)
+    weave_cost, weave_slowdown = run_ablation_slo(trace_fn, 5, 1.3)
+    numbers = list(weave_slowdown.values())
+    print(f"Thpt: "
+          f"colo: {np.mean([colo_thpt[jid] / naived_thpt[jid] for jid in colo_thpt]):.4f}, "
+          f"naive-d: 1, "
+          f"weave: {np.mean(list([1 / sld for sld in weave_slowdown.values()])):.4f}")
+    
+    print("Duration-weighted mean thpt:")
+    print(f"Thpt: "
+          f"colo: {np.sum([colo_thpt[jid] / naived_thpt[jid] * jid_2_duration[jid] for jid in colo_thpt]) / np.sum(list(jid_2_duration.values())):.4f}, "
+          f"naive-d: 1, "
+          f"weave: {np.sum(list([1 / sld  * jid_2_duration[jid] for jid, sld in weave_slowdown.items()])) / np.sum(list(jid_2_duration.values())):.4f}")
+    
+    print(f"Cost: colo: {colo_cost / weave_cost:.4f}, naive-d: {naived_cost / weave_cost:.4f}, weave: 1")
